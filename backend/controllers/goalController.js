7d38f40029995cef92514ae86a450d84ba030766
Jason <snujugroix@gmail.com>
2022-05-19 17:06:02 -0400

    started creating user routes and controller functions

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -49,12 +49,10 @@ const updateGoal = asyncHandler(async (req, res) => {
 // @access Private
 const deleteGoal = asyncHandler(async (req, res) => {
 	const goal = await Goal.findById(req.params.id);
-
 	if (!goal) {
 		res.status(400);
 		throw new Error("Goal not found");
 	}
-
 	await goal.remove();
 
 	res.status(200).json({ id: req.params.id });
