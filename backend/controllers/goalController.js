93b1b2054b90fe503c514d79990b6934fa08aca6
Jason <snujugroix@gmail.com>
2022-05-19 19:39:06 -0400

    created enough react frontend to start redux integration

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -40,6 +40,20 @@ const updateGoal = asyncHandler(async (req, res) => {
 		throw new Error("Goal not found");
 	}
 
+	const user = await User.findById(req.user.id);
+
+	// check for user
+	if (!user) {
+		res.status(401);
+		throw new Error("User not found");
+	}
+
+	// Make sure logged in user matches the goal user
+	if (goal.user.toString() !== user.id) {
+		req.status(401);
+		throw new Error("User not authorized");
+	}
+
 	const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
 		new: true,
 	});
@@ -56,6 +70,20 @@ const deleteGoal = asyncHandler(async (req, res) => {
 		res.status(400);
 		throw new Error("Goal not found");
 	}
+
+	const user = await User.findById(req.user.id);
+
+	// check for user
+	if (!user) {
+		res.status(401);
+		throw new Error("User not found");
+	}
+	// Make sure logged in user matches the goal user
+	if (goal.user.toString() !== user.id) {
+		req.status(401);
+		throw new Error("User not authorized");
+	}
+
 	await goal.remove();
 
 	res.status(200).json({ id: req.params.id });
