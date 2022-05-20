844d75ad917c0b0cf544766621277e9451d4bc37
Jason <snujugroix@gmail.com>
2022-05-20 14:07:50 -0400

    set backend up for production

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -40,16 +40,16 @@ const updateGoal = asyncHandler(async (req, res) => {
 		throw new Error("Goal not found");
 	}
 
-	const user = await User.findById(req.user.id);
+	// const user = await User.findById(req.user.id);
 
 	// check for user
-	if (!user) {
+	if (!req.user) {
 		res.status(401);
 		throw new Error("User not found");
 	}
 
 	// Make sure logged in user matches the goal user
-	if (goal.user.toString() !== user.id) {
+	if (goal.user.toString() !== req.user.id) {
 		req.status(401);
 		throw new Error("User not authorized");
 	}
@@ -71,15 +71,15 @@ const deleteGoal = asyncHandler(async (req, res) => {
 		throw new Error("Goal not found");
 	}
 
-	const user = await User.findById(req.user.id);
+	// const user = await User.findById(req.user.id);
 
 	// check for user
-	if (!user) {
+	if (!req.user) {
 		res.status(401);
 		throw new Error("User not found");
 	}
 	// Make sure logged in user matches the goal user
-	if (goal.user.toString() !== user.id) {
+	if (goal.user.toString() !== req.user.id) {
 		req.status(401);
 		throw new Error("User not authorized");
 	}
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -67,13 +67,9 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route   GET /api/users/me
 // @access  Private
 const getMe = asyncHandler(async (req, res) => {
-	const { _id, name, email } = await User.findById(req.user.id);
+	// const { _id, name, email } = await User.findById(req.user.id);
 
-	res.status(200).json({
-		id: _id,
-		name,
-		email,
-	});
+	res.status(200).json(req.user);
 });
 
 const generateToken = (id) => {
